4f003671283cc4265110303e2d89b376da0c2767
BAUTTO <santiagobb89@gmail.com>
2025-10-09 22:23:19 +0200

    Update genrep.py

--- genrep.py
+++ genrep.py
@@ -2,23 +2,22 @@ print('Ei24 - genrep praktiskt prov HT25')
 
 r_string = input('Ange resistorer: ')
 
-#print(r_string)
-
-if r_string =='':
+if r_string == '':
     print('Serieresistans: 0')
     print('Parallelresistans: 0')
 else:
-r_list = r_string.split()
+    r_list = r_string.split()
 
-r_int = []
-for i in r_list:
-    r_int.append(float(i))
+    r_int = []
+    for i in r_list:
+        r_int.append(float(i))
 
-r_paral = 0
-r_serie =0
-for i in r_int:
-    r_serie = r_serie + i
-    r_paral = r_paral + 1/i
+    r_paral = 0
+    r_serie = 0
+
+    for i in r_int:
+        r_serie = r_serie + i
+        r_paral = r_paral + 1 / i
     
-print(f"Serieresistans:{r_serie}")
-print(f"Parallelresistans:{1/r_paral}")
+    print(f"Serieresistans: {r_serie}")
+    print(f"Parallelresistans: {1 / r_paral}")
