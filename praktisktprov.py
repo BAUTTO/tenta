09b02535680f53055de4560a78a66e4f352a17fe
Santiago <santiagobb89@gmail.com>
2025-10-10 10:52:55 +0200

    Added to the repo on github called tenta

---
+++ praktisktprov.py
@@ -0,0 +1,46 @@
+# Author : Santiago Bautista
+# Date: 2025-10-10
+user_input = input('Vilka tabeller vill du beräkna ?')
+
+r_multiplication_int = [int(i) for i in user_input.split()]
+
+
+for i in r_multiplication_int:
+        print('')
+        print(f"{i} * 1 = {i * 1}")
+        print(f"{i} * 2 = {i * 2}")
+        print(f"{i} * 3 = {i * 3}")
+        print(f"{i} * 4 = {i * 4}")
+        print(f"{i} * 5 = {i * 5}")
+        print(f"{i} * 6 = {i * 6}")
+        print(f"{i} * 7 = {i * 7}")
+        print(f"{i} * 8 = {i * 8}")
+        print(f"{i} * 9 = {i * 9}")
+        print(f"{i} * 10 = {i * 10}")
+        
+        
+        
+
+
+        
+        
+
+
+
+          
+        
+       
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
